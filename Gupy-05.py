42489a9d0c2c1db1ad8bdef419ae438cde1b63d4
Nathan Pagliari <80040544+Nathan-PA@users.noreply.github.com>
2022-09-04 00:13:01 -0300

    Update Gupy-05.py

--- Gupy-05.py
+++ Gupy-05.py
@@ -3,7 +3,7 @@
 #a) Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
 #b) Evite usar funções prontas, como, por exemplo, reverse;
 
-palavra = input(str("Digite a palavra que que inverter:"))
+palavra = input(str("Digite a palavra que quer inverter:"))
 palavra_invertida = "" 
 for i in palavra: 
     palavra_invertida = i + palavra_invertida
