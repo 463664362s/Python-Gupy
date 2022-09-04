7b65e3b2a998ae1558979f1f39c3d65430847788
Nathan-PA <na.pagliariaug@gmail.com>
2022-09-04 00:08:47 -0300

    first commit

---
+++ Gupy-02.py
@@ -0,0 +1,22 @@
+# 2) Dado a sequência de Fibonacci, onde se inicia por 0 e 1 e o próximo valor sempre será a soma dos 2 valores anteriores (exemplo: 0, 1, 1, 2, 3, 5, 8, 13, 21, 34...), 
+# escreva um programa na linguagem que desejar onde, informado um número, ele calcule a sequência de Fibonacci e retorne uma mensagem avisando se o número informado pertence ou não a sequência.
+# IMPORTANTE:
+# Esse número pode ser informado através de qualquer entrada de sua preferência ou pode ser previamente definido no código;
+
+numeroAnterior = -1 
+numeroAtual = 1
+
+numero = int(input("Digite um número para verificar se ele está na sequência: "))
+
+while(1):
+    fibonacci = numeroAnterior + numeroAtual
+    numeroAnterior = numeroAtual
+    numeroAtual = fibonacci
+    if fibonacci == numero:
+        print("O número",numero,"está na sequência de Fibonacci")
+        break
+    if fibonacci > numero:
+        print("O número",numero,"não está na sequência de Fibonacci")
+        break
+
+  
---
+++ Gupy-03.py
@@ -0,0 +1,159 @@
+# 3) Dado um vetor que guarda o valor de faturamento diário de uma distribuidora, faça um programa, na linguagem que desejar, que calcule e retorne:
+# • O menor valor de faturamento ocorrido em um dia do mês;
+# • O maior valor de faturamento ocorrido em um dia do mês;
+# • Número de dias no mês em que o valor de faturamento diário foi superior à média mensal.
+
+# IMPORTANTE:
+# a) Usar o json ou xml disponível como fonte dos dados do faturamento mensal;
+# b) Podem existir dias sem faturamento, como nos finais de semana e feriados. Estes dias devem ser ignorados no cálculo da média;
+
+vetor = [
+	{
+		"dia": 1,
+		"valor": 22174.1664
+	},
+	{
+		"dia": 2,
+		"valor": 24537.6698
+	},
+	{
+		"dia": 3,
+		"valor": 26139.6134
+	},
+	{
+		"dia": 4,
+		"valor": 0.0
+	},
+	{
+		"dia": 5,
+		"valor": 0.0
+	},
+	{
+		"dia": 6,
+		"valor": 26742.6612
+	},
+	{
+		"dia": 7,
+		"valor": 0.0
+	},
+	{
+		"dia": 8,
+		"valor": 42889.2258
+	},
+	{
+		"dia": 9,
+		"valor": 46251.174
+	},
+	{
+		"dia": 10,
+		"valor": 11191.4722
+	},
+	{
+		"dia": 11,
+		"valor": 0.0
+	},
+	{
+		"dia": 12,
+		"valor": 0.0
+	},
+	{
+		"dia": 13,
+		"valor": 3847.4823
+	},
+	{
+		"dia": 14,
+		"valor": 373.7838
+	},
+	{
+		"dia": 15,
+		"valor": 2659.7563
+	},
+	{
+		"dia": 16,
+		"valor": 48924.2448
+	},
+	{
+		"dia": 17,
+		"valor": 18419.2614
+	},
+	{
+		"dia": 18,
+		"valor": 0.0
+	},
+	{
+		"dia": 19,
+		"valor": 0.0
+	},
+	{
+		"dia": 20,
+		"valor": 35240.1826
+	},
+	{
+		"dia": 21,
+		"valor": 43829.1667
+	},
+	{
+		"dia": 22,
+		"valor": 18235.6852
+	},
+	{
+		"dia": 23,
+		"valor": 4355.0662
+	},
+	{
+		"dia": 24,
+		"valor": 13327.1025
+	},
+	{
+		"dia": 25,
+		"valor": 0.0
+	},
+	{
+		"dia": 26,
+		"valor": 0.0
+	},
+	{
+		"dia": 27,
+		"valor": 25681.8318
+	},
+	{
+		"dia": 28,
+		"valor": 1718.1221
+	},
+	{
+		"dia": 29,
+		"valor": 13220.495
+	},
+	{
+		"dia": 30,
+		"valor": 8414.61
+	}
+]
+soma_faturamento = 0
+faturamento_diario_maior = 0
+dias_de_faturamento = 0
+
+vetor_sem_0 = []
+
+for n in range(len(vetor)):
+	if  vetor[n]['valor'] > 0:
+		soma_faturamento = vetor[n]['valor'] + soma_faturamento
+		dias_de_faturamento = dias_de_faturamento + 1
+		vetor_sem_0.append(vetor[n])
+
+
+def maiorfaturamento(vetor):
+    return max(vetor, key=lambda d : d['valor'])
+
+def menorfaturamento(vetor):
+    return min(vetor, key=lambda d : d['valor'])
+
+media_anual = soma_faturamento/dias_de_faturamento
+
+for n in range(len(vetor)):
+	if vetor[n]['valor'] > media_anual:
+		faturamento_diario_maior = faturamento_diario_maior + 1
+		
+print("Maior faturamento: ",maiorfaturamento(vetor)) 
+print("Menor faturamento: ",menorfaturamento(vetor_sem_0)) 
+print("Dias que o faturamento foi maior que a media: ",faturamento_diario_maior)
